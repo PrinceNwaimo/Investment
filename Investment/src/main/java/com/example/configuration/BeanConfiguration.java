0f6a8a5a9d4375681e758019e18f305c11991126
Prince Nwaimo <nwaimoprince12@gmail.com>
2024-06-11 14:48:26 +0100

    for main

--- Investment/src/main/java/com/example/configuration/BeanConfiguration.java
+++ Investment/src/main/java/com/example/configuration/BeanConfiguration.java
@@ -1,20 +1,18 @@
 package com.example.configuration;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import com.example.utils.JwtUtils;
 
 
 
-import static com.example.utils.AppUtils.JWT_SIGNING_SECRET;
+
+
 @Configuration
 public class BeanConfiguration {
-    @Value(JWT_SIGNING_SECRET)
-    private String jwt_secret;
+
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
@@ -24,22 +22,8 @@ public class BeanConfiguration {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    public JwtUtils jwtUtils(){
-        return new JwtUtils(jwt_secret);
-    }
 
-    @Value("${spring.mail.host}")
-    private String mailHost;
-    @Value("${spring.mail.port}")
-    private int mailPort;
-    @Value("${spring.mail.protocol}")
-    private String mailProtocol;
-    @Value("${spring.mail.password}")
-    private String mailPassword;
-    @Value("${spring.mail.username}")
-    private String mailUsername;
-    @Value("jwt.signing.secret")
-    private String secret;
+
+
 
 }
--- Investment/src/main/java/com/example/utils/AppUtils.java
+++ Investment/src/main/java/com/example/utils/AppUtils.java
@@ -46,7 +46,7 @@ public class AppUtils {
 
         public static final String LOGIN_ENDPOINT = "/api/v1/login";
         public static final String AGENT_API_VALUE = "/api/v1/agent";
-        public static final String JWT_SIGNING_SECRET = "${sendinblue.api.key}";
+
 
 
         public static Pageable buildPageRequest(int page, int items){
@@ -67,85 +67,9 @@ public class AppUtils {
                     AGENT_API_VALUE, LOGIN_ENDPOINT
             );
         }
-//        public static List<SimpleGrantedAuthority> getAuthorities(Collection<? extends GrantedAuthority> authorities) {
-//            return authorities
-//                    .stream()
-//                    .map(grantedAuthority -> new SimpleGrantedAuthority(grantedAuthority.getAuthority()))
-//                    .toList();
-//        }
 
-        public  static String buildWelcomeEmail(String userName, String companyName, String platformName) {
-            String emailTemplate = "<!DOCTYPE html>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "    <title>Welcome to " + companyName + "!</title>\n" +
-                    "    <style>\n" +
-                    "        body {\n" +
-                    "            font-family: Arial, sans-serif;\n" +
-                    "            line-height: 1.6;\n" +
-                    "            margin: 0;\n" +
-                    "            padding: 0;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        .container {\n" +
-                    "            max-width: 600px;\n" +
-                    "            margin: 0 auto;\n" +
-                    "            padding: 20px;\n" +
-                    "            background-color: #f2f2f2;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        h1 {\n" +
-                    "            color: #0066cc;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        p {\n" +
-                    "            margin-bottom: 20px;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        ul {\n" +
-                    "            margin-bottom: 20px;\n" +
-                    "            list-style-type: disc;\n" +
-                    "            padding-left: 20px;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        li {\n" +
-                    "            margin-bottom: 10px;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        .button {\n" +
-                    "            display: inline-block;\n" +
-                    "            background-color: #0066cc;\n" +
-                    "            color: #fff;\n" +
-                    "            padding: 10px 20px;\n" +
-                    "            text-decoration: none;\n" +
-                    "            border-radius: 5px;\n" +
-                    "        }\n" +
-                    "\n" +
-                    "        .button:hover {\n" +
-                    "            background-color: #0052a3;\n" +
-                    "        }\n" +
-                    "    </style>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "    <div class=\"container\">\n" +
-                    "        <h1>Welcome to " + companyName + "!</h1>\n" +
-                    "        <p>Dear " + userName + ",</p>\n" +
-                    "        <p>Welcome to " + companyName + "! We are thrilled to have you as a part of our community.</p>\n" +
-                    "        <p>Thank you for joining us and taking the first step towards making a positive impact on the environment. Our mission is to promote sustainable practices and foster a greener future, and your presence is vital in achieving that goal.</p>\n" +
-                    "        <p>With " + platformName + ", you can actively participate in plastic waste collection and recycling efforts in your community. Together, we can make a significant difference in reducing plastic pollution and creating a cleaner, healthier planet.</p>\n" +
-                    "        <p>Here are a few things you can do on our platform:</p>\n" +
-                    "        <ul>\n" +
-                    "            <li>Submit Waste Collection Requests: Easily request waste collection services for recycling plastic waste from your location.</li>\n" +
-                    "            <li>Track Collection Status: Stay updated on the status of your waste collection requests in real-time.</li>\n" +
-                    "            <li>Explore Recycling Centers: Discover nearby recycling centers where you can drop off recyclable plastic materials.</li>\n" +
-                    "            <li>Make Payments: Conveniently make payments for waste collection services through our secure payment system.</li>\n" +
-                    "            <li>Stay Informed: Receive important notifications and updates about the latest recycling initiatives and environmental news.</li>\n" +
-                    "        </ul>\n" +
-                    "        <p>We are committed to providing you with a seamless and enjoyable experience on our platform. Should you have any questions, feedback, or concerns, please don't hesitate to reach out to our customer support team. Your satisfaction is our top priority.</p>\n" +
-                    "  " +
-                    "      <p>Once again, welcome aboard! Your dedication to environmental conservation and sustainable practices is highly valued, and we look forward to embarking on this journey together.</p>\n";
-            return emailTemplate;
-        }
+
+
 
     }
 
